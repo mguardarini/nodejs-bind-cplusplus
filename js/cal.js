e12cb09421037795521b703afacbe5b829ffc7d1
Mauro Guardarini <mauro.guardarini@gmail.com>
2018-11-25 21:28:44 -0200

    changed the name of main js

--- js/cal.js
+++
@@ -1,5 +0,0 @@
-import {Calculator} from "./math";
-
-
-let result = new Calculator(12,12);
-console.log('5+2='+result.Add());
---
+++ js/index.js
@@ -0,0 +1,8 @@
+import {Calculator} from "./math";
+
+
+let result = new Calculator();
+
+console.log('5+2='+result.Add(5,2));
+console.log('5+2='+result.multiply(5,2));
+console.log('5+2='+result.minus(5,2));
